73d707f66742e4bc172ad3835c97db400c8f03d3
Dzmitry Lopukh <lopukh.d.a@gmail.com>
2021-08-31 17:43:17 +0300

    Created definitions

---
+++ LearnSpecFlow/Steps/MainMenuSteps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TechTalk.SpecFlow;
+
+namespace LearnSpecFlow.Steps
+{
+	[Binding]
+	public sealed class MainMenuSteps
+	{
+		// For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+		private readonly ScenarioContext _scenarioContext;
+
+		public MainMenuSteps(ScenarioContext scenarioContext)
+		{
+			_scenarioContext = scenarioContext;
+		}
+
+		[Given(@"I open official Specflow web site")]
+		public void OpenOfficialSpecflowWebSite()
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I hover the '(.*)' menu item from the main menu")]
+		public void HoverTheMenuItemFromTheMainMenu(string menuItem)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[When(@"I click the '(.*)' option from sub menu")]
+		public void ClickTheOptionFromSubMenu(string option)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+		[Then(@"Page with '(.*)' title should be opened")]
+		public void PageWithTitleShouldBeOpened(string title)
+		{
+			ScenarioContext.Current.Pending();
+		}
+
+	}
+}
