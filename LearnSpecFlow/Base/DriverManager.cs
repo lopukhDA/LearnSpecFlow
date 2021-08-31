7c8cf31137fb9dba115c97dc460b52035d1d676c
Dzmitry Lopukh <lopukh.d.a@gmail.com>
2021-08-31 17:42:41 +0300

    DriverManager

---
+++ LearnSpecFlow/Base/DriverManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace LearnSpecFlow.Base
+{
+	public class DriverManager
+    {
+        private static IWebDriver driver;
+
+        public static IWebDriver Instance()
+        {
+            if (driver == null)
+            {
+                driver = new ChromeDriver();
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+                driver.Manage().Window.Maximize();
+            }
+
+            return driver;
+        }
+
+        public static void QuitDriver()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
